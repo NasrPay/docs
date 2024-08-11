ad7274ca051b644e11f70d5506d00f84cb590225
Babak <babakcode@gmail.com>
2024-08-11 18:12:27 +0330

    * features 1. redirect url 2. enterprise gateways

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -39,8 +39,7 @@ const config: Config = {
           sidebarPath: './sidebars.ts',
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            'https://github.com/nasrpay/docs/tree/main/packages/create-docusaurus/templates/shared/',
+          editUrl: 'https://github.com/nasrpay/docs/tree/master',
         },
         // blog: {
         //   showReadingTime: true,
